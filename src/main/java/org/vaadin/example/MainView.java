5111c19283533c6dc240b938e1060b864ae75b8e
drwiner <drwiner131@gmail.com>
2022-01-07 13:34:51 -0500

    datagrid and databind

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -1,41 +1,58 @@
 package org.vaadin.example;
 
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLayout;
+import com.vaadin.flow.router.RouterLink;
+import org.vaadin.example.databind.BindingForms;
+import org.vaadin.example.databind.Validation;
+import org.vaadin.example.datagrid.BackEndDataProvider;
 
 /**
  * The main view contains a text field for getting the user name and a button
  * that shows a greeting message in a notification.
  */
-@Route("")
-public class MainView extends VerticalLayout {
+//@Route("")
+public class MainView extends AppLayout implements RouterLayout {
 
     public MainView() {
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-        textField.addThemeName("bordered");
-        // Button click listeners can be defined as lambda expressions
-        GreetService greetService = new GreetService();
-        Button button = new Button("Say hello 2", e -> Notification
-                .show(greetService.greet(textField.getValue())));
+        addToNavbar(new DrawerToggle());
+        addToNavbar(new H2("Data Binding Exercise") );
 
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button is more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        final VerticalLayout menuBar = new VerticalLayout();
+        menuBar.add(new RouterLink(Validation.TITLE, Validation.class));
+        menuBar.add(new RouterLink(BindingForms.TITLE, BindingForms.class));
 
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
+        menuBar.add(new RouterLink(BackEndDataProvider.TITLE, BackEndDataProvider.class));
 
-        // Use custom CSS classes to apply styling. This is defined in
-        // shared-styles.css.
-        addClassName("centered-content");
+        addToDrawer(menuBar);
 
-        add(textField, button);
     }
+
+//    public MainView() {
+//        // Use TextField for standard text input
+//        TextField textField = new TextField("Your name");
+//        textField.addThemeName("bordered");
+//        // Button click listeners can be defined as lambda expressions
+//        GreetService greetService = new GreetService();
+//        Button button = new Button("Say hello", e -> Notification
+//                .show(greetService.greet(textField.getValue())));
+//
+//        // Theme variants give you predefined extra styles for components.
+//        // Example: Primary button is more prominent look.
+//        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+//
+//        // You can specify keyboard shortcuts for buttons.
+//        // Example: Pressing enter in this view clicks the Button.
+//        button.addClickShortcut(Key.ENTER);
+//
+//        // Use custom CSS classes to apply styling. This is defined in
+//        // shared-styles.css.
+//        addClassName("centered-content");
+//
+//        add(textField, button);
+//    }
 }
